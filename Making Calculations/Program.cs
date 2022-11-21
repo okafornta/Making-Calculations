0fe91b694c152d0a8e6b6e71df4b91aff87cfc18
Okafornta <hillaryonia@gmail.com>
2022-11-21 09:07:16 +0100

    Add project files.

---
+++ Making Calculations/Program.cs
@@ -0,0 +1,12 @@
+﻿// Writing a code that will display to the user what 1 plus 1 is = 2 and 3
+// output of a calculation
+Console.WriteLine(1 + 1);
+
+Console.WriteLine(2 + 1);
+
+// Waiting for Enter
+Console.ReadLine();
+
+/* NOTE: In programming, this kind of calculation (generally, a combination of values) is called an
+expression.
+*/
